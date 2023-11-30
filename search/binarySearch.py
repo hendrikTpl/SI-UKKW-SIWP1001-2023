f0c57e55f42df4019bdab7b331d1994139dc8fe2
katherin7 <monicakatherin75@gmail.com>
2023-11-30 11:32:24 +0700

    initilize homework2

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -1,33 +1,50 @@
-"""
-Diberikan sebuah array berisi integer nums yang telah diurutkan secara ascending, 
-serta sebuah integer target, tuliskan sebuah fungsi untuk mencari target di dalam nums. 
-Jika target ada, maka kembalikan indeksnya. Jika tidak, kembalikan -1.
+# """
+# Diberikan sebuah array berisi integer nums yang telah diurutkan secara ascending, 
+# serta sebuah integer target, tuliskan sebuah fungsi untuk mencari target di dalam nums. 
+# Jika target ada, maka kembalikan indeksnya. Jika tidak, kembalikan -1.
 
-algorithma binarySearch harus mencapai O(log n) runtime complexity.
+# algorithma binarySearch harus mencapai O(log n) runtime complexity.
 
-Contoh 1:
-Input: nums = [-1,0,3,5,9,12], target = 9
-Output: 4
-Penjelasan: 9 ada di nums dan indeksnya adalah 4
+# Contoh 1:
+# Input: nums = [-1,0,3,5,9,12], target = 9
+# Output: 4
+# Penjelasan: 9 ada di nums dan indeksnya adalah 4
 
-Contoh 2:
-Input: nums = [-1,0,3,5,9,12], target = 2
-Output: -1
-Penjelasan: 2 tidak ada di nums jadi kembalikan -1
+# Contoh 2:
+# Input: nums = [-1,0,3,5,9,12], target = 2
+# Output: -1
+# Penjelasan: 2 tidak ada di nums jadi kembalikan -1
 
-Batasan:
-1 <= panjang nums <= 10^4
--10^4 < nums[i], target < 10^4
-semua angka harus integer unik
-semua angka di urutkan secara ascending
+# Batasan:
+# 1 <= panjang nums <= 10^4
+# -10^4 < nums[i], target < 10^4
+# semua angka harus integer unik
+# semua angka di urutkan secara ascending
 
-"""
-from typing import List
-from util.decorators import time_decorator
+# """
+# from typing import List
+# from util.decorators import time_decorator
+    # @time_decorator
 
-class BinarySearch():
-    @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+def binarySearch(nums, target):
+    left, right = 0, len(nums) - 1
+    
+    while left <= right:
+        mid = left + (right - left) // 2
+        
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
 
+number = [-1, 0, 3, 5, 9, 12]
+target = 9
+print(binarySearch(number, target))
+
+number2 = [-1, 0, 3, 5, 9, 12]
+target2 = 2
+print(binarySearch(number2, target2))
--- search/linearSearch.py
+++ search/linearSearch.py
@@ -1,12 +1,38 @@
 """
-Buat sebuag algorithm LinearSeach
+Buat sebuah algorithm LinearSeach
+
+definisikan linear search terlebih dahulu 
+lalu buat fungsi untuk mencari target 
+tuliskan sebuah fungsi untuk mencari target di dalam nums. 
+Jika target ada, maka kembalikan indeksnya. Jika tidak, kembalikan -1.
 
 """
-from typing import List
-from util.decorators import time_decorator
+# from typing import List
+# from util.decorators import time_decorator
 
-class LinearSearch():
-    @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  
+    return -1  
+
+numbers = [4, 2, 5, 1, 7, 3, 6, 8]
+target = 5
+
+result = linear_search(numbers, target)
+
+if result != -1:
+    print(f"Target {target} found at index: {result}")
+else:
+    print(f"Target {target} not found in the array.")
+
+numbers = [4, 2, 5, 1, 7, 3, 6, 8]
+target = 9
+
+result = linear_search(numbers, target)
+
+if result != -1:
+    print(f"Target {target} found at index: {result}")
+else:
+    print(f"Target {target} not found in the array.")
+    pass
