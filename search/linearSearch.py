3e3babd060d214f0b9fee95d54083c2d17eb47de
willyrusli <150768224+willyrusli@users.noreply.github.com>
2023-12-23 00:21:33 +0700

    Update linearSearch.py

--- search/linearSearch.py
+++ search/linearSearch.py
@@ -10,3 +10,8 @@ class LinearSearch():
     def search(self, nums: List[int], target: int) -> int:
         # write your code here
         pass
+ main.py LinearSearch --indata "<1,2,3,4,5,6,7,8,9,10,11>" target:5
+    data ditemukan!!!
+    data terdapat di indeks:5
+main.py LinearSearch --indata "<1,2,3,4,5,6,7,8,9,10,11>" target:15
+    data tidak tersedia
