2a64512a4b400d8902aec87cc1984255483a90d4
unknown <steven.422023029@civitas.ukrida.ac.id>
2023-12-23 00:16:33 +0700

    hw#2

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -22,12 +22,54 @@ semua angka harus integer unik
 semua angka di urutkan secara ascending
 
 """
-from typing import List
+import sys
+import os
+current_script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_script_path))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 from util.decorators import time_decorator
 
-class BinarySearch():
-    @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+import time
 
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    start_time = time.time()  # Catat waktu mulai eksekusi
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        # Jika elemen tengah adalah target, kembalikan indeksnya
+        if arr[mid] == target:
+            end_time = time.time()  # Catat waktu berakhir eksekusi jika elemen ditemukan
+            runtime = end_time - start_time
+            return mid, runtime
+        
+        # Jika elemen tengah lebih kecil dari target, cari di setengah atas
+        elif arr[mid] < target:
+            low = mid + 1
+        
+        # Jika elemen tengah lebih besar dari target, cari di setengah bawah
+        else:
+            high = mid - 1
+    
+    # Jika target tidak ditemukan, kembalikan -1
+    end_time = time.time()  # Catat waktu berakhir eksekusi jika elemen tidak ditemukan
+    runtime = end_time - start_time
+    return -1, runtime
+
+# Contoh penggunaan binary search
+angka = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+target = 10
+
+hasil, waktu_eksekusi = binary_search(angka, target)
+
+if hasil != -1:
+    print("Angka", target, "ditemukan pada indeks ke", hasil)
+else:
+    print("Angka", target, "tidak ditemukan dalam array")
+
+print(f"Waktu eksekusi: {waktu_eksekusi} detik.")
--- search/linearSearch.py
+++ search/linearSearch.py
@@ -2,11 +2,39 @@
 Buat sebuag algorithm LinearSeach
 
 """
-from typing import List
+import sys
+import os
+current_script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_script_path))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 from util.decorators import time_decorator
 
-class LinearSearch():
-    @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+import time
+
+def linear_search(data, target):
+    start_time = time.time()  # Catat waktu mulai eksekusi
+    for i in range(len(data)):
+        if data[i] == target:
+            end_time = time.time()  # Catat waktu berakhir eksekusi jika elemen ditemukan
+            runtime = end_time - start_time
+            return i, runtime
+    end_time = time.time()  # Catat waktu berakhir eksekusi jika elemen tidak ditemukan
+    runtime = end_time - start_time
+    return -1, runtime
+
+# Contoh penggunaan
+data = [1, 3, 5, 7, 9, 11, 13]
+target = 9
+
+hasil_pencarian, waktu_eksekusi = linear_search(data, target)
+
+if hasil_pencarian != -1:
+    print(f"Elemen {target} ditemukan pada indeks {hasil_pencarian}.")
+else:
+    print(f"Elemen {target} tidak ditemukan dalam data.")
+
+print(f"Waktu eksekusi: {waktu_eksekusi} detik.")
+
+
