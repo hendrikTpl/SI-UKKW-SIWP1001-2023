655193b79c5f8520e52637bb6c816a352350b8b3
StevenWijaya422023002 <steven.422023002@civitas.ukrida.ac.id>
2023-12-25 14:18:54 +0700

    tugas algo

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -22,12 +22,50 @@ semua angka harus integer unik
 semua angka di urutkan secara ascending
 
 """
-from typing import List
-from util.decorators import time_decorator
 
-class BinarySearch():
-    @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+
+
+import sys
+import os
+current_script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_script_path))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+from util.time_measurement import time_function,  CodeTimer
+
+
+import time
+
+def binarySearch(arr, target):
+    start_time = time.time()
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) //2
+
+        if arr[mid] == target:
+            end_time = time.time()
+            runtime = end_time - start_time
+            return mid, runtime
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1 
+
+    end_time = time.time()
+    runtime = end_time - start_time
+    return -1, runtime
+
+number = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+target = 12
+
+hasil, waktu_eksekusi = binarySearch(number, target)
+
+if hasil != -1:
+    print("Number", target, "berada di indeks ke", hasil)
+else:
+    print("Number", target, "tidak ditemukan")
+
+print(f"Waktu eksekusi: {waktu_eksekusi} detik.")
 
--- search/linearSearch.py
+++ search/linearSearch.py
@@ -2,11 +2,41 @@
 Buat sebuag algorithm LinearSeach
 
 """
-from typing import List
-from util.decorators import time_decorator
+import sys
+import os
+current_script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_script_path))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+from util.time_measurement import time_function,  CodeTimer
 
-class LinearSearch():
-    @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+import time
+
+class LinearSearch:
+    def time_function(func):
+        def wrapper(self, List_1, item):
+            start_time = time.time()
+            result = func(self, List_1, item)
+            end_time = time.time()
+            runtime = end_time - start_time
+            print(f"waktu eksekusi: {runtime} detik")
+            return result
+        return wrapper
+
+    @time_function
+    def linear(self, List_1, item):
+        for i in range(len(List_1)):
+            if item == List_1[i]:
+                print("Item ada di index ke:", i)
+                break
+        else:
+            print("Item tidak ditemukan")
+
+
+linear_search = LinearSearch()
+
+List_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+item = 5
+
+linear_search.linear(List_1, item)
+
