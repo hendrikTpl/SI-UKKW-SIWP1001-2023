1998875531df0c49344a8daf57d2c9d5dda77f94
Victorioputra <victorio.422023022@civitas.ukrida.ac.id>
2023-12-27 22:17:33 +0700

    hw2victorio

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -29,5 +29,20 @@ class BinarySearch():
     @time_decorator
     def search(self, nums: List[int], target: int) -> int:
         # write your code here
-        pass
-
+        
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return -1
+        
+Bsr = BinarySearch()
+print(Bsr.search([-1,0,3,5,9,12],9))
+print(Bsr.search([-1,0,3,5,9,12],2))
