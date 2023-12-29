3fe99641528ee88fdb9cd1d794617399571e2082
bambangajaya <bambangaja2004@gmail.com>
2023-12-29 21:13:26 +0700

    #hw2

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -22,12 +22,48 @@ semua angka harus integer unik
 semua angka di urutkan secara ascending
 
 """
-from typing import List
+import sys
+import os
+current_script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_script_path))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+from util.time_measurement import time_function,  CodeTimer
+#measure the line of code
+with CodeTimer('binary'):
+    for i in range(10):
+        #do something
+        pass
+    from typing import List
 from util.decorators import time_decorator
+#measure function
 
 class BinarySearch():
     @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
+    @time_function  
+    def binary_Search(self, nums: List[int], target: int) -> int:
         # write your code here
-        pass
+        low, high = 0, len(nums) - 1
 
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return -1
+    
+bs = BinarySearch()
+
+nums = [4, 5, 6, 7, 0, 1, 2]
+target = 5
+index = bs.binary_Search(nums, target)
+
+if index != -1:
+    print(f"Element found at index {index}")
+else:
+    print("Element not found in the list")
--- search/linearSearch.py
+++ search/linearSearch.py
@@ -2,11 +2,40 @@
 Buat sebuag algorithm LinearSeach
 
 """
-from typing import List
+import sys
+import os
+current_script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_script_path))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+from util.time_measurement import time_function,  CodeTimer
+#measure the line of code
+with CodeTimer('tugas'):
+    for i in range(10):
+        #do something
+        pass
+    from typing import List
 from util.decorators import time_decorator
+#measure function
+ 
+
 
 class LinearSearch():
     @time_decorator
+    @time_function  
     def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+
+ls = LinearSearch()
+
+# Example usage:
+nums = [4, 5, 6, 7, 0, 1, 2]
+target = 0
+index = ls.search(nums, target)
+
+if index != -1:
+    print(f"Element found at index {index}")
+else:
+    print("Element not found in the list")
