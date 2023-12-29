580729e5afab52ee0f206607a183b87b7d2b1c21
mjayy77 <billymj77@gmail.com>
2023-12-29 19:55:56 +0700

    testing_testing

--- search/binarySearch.py
+++ search/binarySearch.py
@@ -22,12 +22,48 @@ semua angka harus integer unik
 semua angka di urutkan secara ascending
 
 """
-from typing import List
+import sys
+import os
+current_script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_script_path))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 from util.decorators import time_decorator
 
-class BinarySearch():
-    @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+import time
 
+@time_decorator
+def binary_search(array, target):
+    low = 0
+    high = len(array) - 1
+    
+    start_time = time.perf_counter() 
+
+    while low <= high:
+        mid = (low + high) // 2
+        if array[mid] == target:
+            end_time = time.perf_counter()
+            runtime = (end_time - start_time) * 1000.0
+            return mid, runtime
+        
+        elif array[mid] < target:
+            low = mid + 1
+            
+        else:
+            high = mid - 1
+            
+    end_time = time.time()
+    runtime = (end_time - start_time) * 1000.0
+    return -1, runtime
+
+array = [4, 16, 36, 64, 100, 144, 196, 256, 324, 400]
+target = 144
+result, time_of_execution = binary_search(array, target)
+
+if result != -1:
+    print(f"Int. found at index {result}")
+else:
+    print("Int. not found")
+    
+    print(f"Execution time of {binary_search.time_of_execution}: seconds")
--- search/linearSearch.py
+++ search/linearSearch.py
@@ -2,11 +2,41 @@
 Buat sebuag algorithm LinearSeach
 
 """
-from typing import List
+import sys
+import os
+current_script_path = os.path.abspath(__file__)
+project_root = os.path.dirname(os.path.dirname(current_script_path))
+if project_root not in sys.path:
+    sys.path.append(project_root)
+
 from util.decorators import time_decorator
 
-class LinearSearch():
-    @time_decorator
-    def search(self, nums: List[int], target: int) -> int:
-        # write your code here
-        pass
+import time
+
+@time_decorator
+def LinearSearch(array, n, x):
+
+    start_time = time.perf_counter()
+
+    for i in range(0, n):
+        if (array[i] == x):
+            end_time = time.perf_counter() 
+            runtime = (end_time - start_time) * 1000.0
+            return i, runtime
+        end_time = time.perf_counter()
+    runtime = end_time - start_time
+    return -1, runtime
+
+
+array = [2, 4, 0, 1, 9]
+x = 1
+n = len(array)
+
+result, time_of_execution = LinearSearch(array, n, x)
+
+if(result == -1):
+    print("Element not found")
+else:
+    print("Element found at index: ", result)
+
+    print(f"Execution time of {time_of_execution}: seconds")
